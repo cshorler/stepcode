2b51585abf8df61014c92b09f4b0048c065dbc3e
Chris HORLER <cshorler@googlemail.com>
2020-03-25 15:11:25 +0000

    add missing database tables, indexes.  cleanup

--- src/exp2python/python/SCL/cPart21.py
+++ src/exp2python/python/SCL/cPart21.py
@@ -1,5 +1,5 @@
 import logging
-import sqlite3
+import os.path, sqlite3, datetime, tempfile
 
 import re
 import ply.lex as lex
@@ -17,10 +17,47 @@ p21_binary = r'(?:"[0-3][0-9A-F]*")'
 p21_enumeration = r'(?:\.[A-Z_][A-Z0-9_]*\.)'
 p21_keyword = r'(?:!|)[A-Za-z_][0-9A-Za-z_]*'
 p21_eid = r'\#[0-9]+'
-p21_literals = r'(?:[(),*$])'
-_types = [p21_real, p21_integer, p21_string, p21_binary, p21_enumeration,
-          p21_keyword, p21_eid, p21_literals]
-catchall_re = '(?:' + '|'.join(_types) + ')+'
+
+_catchall_types = [p21_real, p21_integer, p21_string, p21_binary, p21_enumeration,
+                   p21_keyword, p21_eid]
+
+groups_re = re.compile(r"(?P<lparens>\()|(?P<eid>\#[0-9]+)|(?P<rparens>\))|(?P<string>')")
+p21_string_re = re.compile(p21_string)
+
+def _mkgroups(s):
+    """used to populate the xref database table"""
+    stack_idx = 0
+    stack_depth = 2
+    stack = [set(), set()]
+    
+    cp = 0
+    while True:
+        m = groups_re.search(s, cp)
+        if not m: break
+    
+        if m.group('eid'):
+            stack[stack_idx].add(m.group())
+        elif m.group('lparens'):
+            stack_idx += 1
+            if stack_idx == len(stack):
+                stack_depth += 2
+                stack.extend((set(), set()))
+        elif m.group('rparens'):
+            stack_idx -= 1
+        else:
+            m = p21_string_re.match(s, m.start())
+            
+        cp = m.end()
+
+    groups = []
+    if any(stack):
+        _stack = filter(bool, stack)
+        # expand the first level
+        stack = tuple((x,) for x in next(_stack)) + tuple(_stack)
+        groups = list(enumerate(stack, 1))
+        
+    return groups
+
 
 base_tokens = ['PART21_START', 'PART21_END', 'HEADER', 'DATA', 'ENDSEC',
                'INTEGER', 'REAL', 'KEYWORD', 'STRING', 'BINARY', 'ENUMERATION',
@@ -33,10 +70,8 @@ class Lexer(object):
     tokens = list(base_tokens)
     literals = '()=;,*$'
 
-    # TODO: this can probably be done with less states
     states = (('slurp', 'exclusive'),
               ('header', 'exclusive'),
-              ('dataparams', 'exclusive'),
               ('data', 'exclusive'),
               ('raw', 'exclusive'),
               ('params', 'exclusive'))
@@ -129,62 +164,39 @@ class Lexer(object):
         return t
     def t_DATA(self, t):
         r'DATA\b'
-        t.lexer.push_state('dataparams')
+        t.lexer.in_header = True
+        t.lexer.push_state('data')
         return t
 
 
     @TOKEN(p21_keyword)
     def t_header_KEYWORD(self, t):
         return t
-    def t_dataparams_header_lparens(self, t):
+    def t_header_lparens(self, t):
         r'\('
-        t.type = '('
-        t.lexer.lvl += 1
+        t.lexer.lexpos -= 1
         t.lexer.push_state('params')
-        return t
-    def t_dataparams_semi(self, t):
-        r';'
-        t.type = ';'
-        t.lexer.pop_state()
-        t.lexer.push_state('data')
-        return t
-
-
-    @TOKEN(p21_integer)
-    def t_params_INTEGER(self, t):
-        return t
-    @TOKEN(p21_real)
-    def t_params_REAL(self, t):
-        return t
-    @TOKEN(p21_keyword)
-    def t_params_KEYWORD(self, t):
-        return t
-    @TOKEN(p21_string)
-    def t_params_STRING(self, t):
-        return t
-    @TOKEN(p21_binary)
-    def t_params_BINARY(self, t):
-        return t
-    @TOKEN(p21_enumeration)
-    def t_params_ENUMERATION(self, t):
-        return t
-    @TOKEN(p21_eid)
-    def t_params_EID(self, t):
-        return t
-    def t_params_lparens(self, t):
-        r'\('
-        t.type = '('
-        t.lexer.lvl += 1
-        return t
-    def t_params_rparens(self, t):
+    def t_header_rparens(self, t):
         r'\)'
         t.type = ')'
-        t.lexer.lvl -= 1
-        if t.lexer.lvl == 0:
-            t.lexer.pop_state()
+        t.lexer.pop_state()
         return t
 
 
+    def t_data_lparens(self, t):
+        r'\('
+        if t.lexer.in_header:
+            t.type = '('
+            t.lexer.push_state('header')
+        else:
+            t.type = 'RAW'
+            t.lexer.push_state('raw')
+        return t
+    def t_data_header_end(self, t):
+        r';'
+        t.type = ';'
+        t.lexer.in_header = False
+        return t
     @TOKEN(p21_eid)
     def t_data_EID(self, t):
         return t
@@ -192,65 +204,39 @@ class Lexer(object):
     def t_data_KEYWORD(self, t):
         t.lexer.push_state('raw')
         return t
-    def t_data_lparens(self, t):
-        r'\('
-        t.lexer.lexpos -= 1
-        t.lexer.push_state('raw')
 
-    @TOKEN(catchall_re)
-    def t_raw_RAW(self, t):
+
+    def t_params_lparens(self, t):
+        r'\('
+        t.type = 'RAW'
+        t.lexer.lvl += 1
         return t
+    def t_params_rparens(self, t):
+        r'\)'
+        t.type = 'RAW'
+        t.lexer.lvl -= 1
+        if t.lexer.lvl == 0:
+            t.lexer.pop_state()
+        return t
+    @TOKEN('(?:' + '|'.join(_catchall_types) + r'|(?:[,*$]))+')
+    def t_params_RAW(self, t):
+        return t
+    
+
     def t_raw_end(self, t):
         r';'
         t.lexer.pop_state()
         t.type = ';'
         return t
+    @TOKEN('(?:' + '|'.join(_catchall_types) + r'|(?:[(),*$]))+')
+    def t_raw_RAW(self, t):
+        return t
+
 
     def t_ANY_newline(self, t):
         r'\n+'
         t.lexer.lineno += len(t.value)    
     t_ANY_ignore = ' \t\r'
-
-
-####################################################################################################
-# Simple Model
-####################################################################################################
-class P21File:
-    def __init__(self, header, *sections):
-        self.header = header
-        self.sections = list(*sections)
-
-class P21Header:
-    def __init__(self, file_description, file_name, file_schema):
-        self.file_description = file_description
-        self.file_name = file_name
-        self.file_schema = file_schema
-        self.extra_headers = []
-
-class HeaderEntity:
-    def __init__(self, type_name, *params):
-        self.type_name = type_name
-        self.params = list(params) if params else []
-
-class Section:
-    def __init__(self, entities):
-        self.entities = entities
-
-class SimpleEntity:
-    def __init__(self, ref, type_name, *params):
-        self.ref = ref
-        self.type_name = type_name
-        self.params = list(params) if params else []
-
-class ComplexEntity:
-    def __init__(self, ref, *params):
-        self.ref = ref
-        self.params = list(params) if params else []
-
-class TypedParameter:
-    def __init__(self, type_name, *params):
-        self.type_name = type_name
-        self.params = list(params) if params else None
 
 
 ####################################################################################################
@@ -260,18 +246,18 @@ class Parser(object):
     tokens = list(base_tokens)
     start = 'exchange_file'
     
-    def __init__(self, lexer=None, debug=0):
+    def __init__(self, lexer=None, debug=0, tempdb=False):
         self.lexer = lexer if lexer else Lexer()
+        self.tempdb = tempdb
 
         try: self.tokens = lexer.tokens
         except AttributeError: pass
 
         self.parser = yacc.yacc(module=self, debug=debug, debuglog=logger, errorlog=logger)
-        self.reset()
     
-    def parse(self, p21_data, **kwargs):
+    def parse(self, p21_data, db_path=None, **kwargs):
         #TODO: will probably need to change this function if the lexer is ever to support t_eof
-        self.lexer.reset()
+        self.reset(self.tempdb, db_path)
         self.lexer.input(p21_data)
 
         if 'debug' in kwargs:
@@ -281,71 +267,133 @@ class Parser(object):
             result = self.parser.parse(lexer=self.lexer, **kwargs)
         return result
 
-    def reset(self):
-        self.closedb()
-        self.initdb()
+    def reset(self, tempdb=None, db_path=None):
+        self.initdb(tempdb, db_path)
+
+    def closedb(self):
+        try:
+            self.db_cxn.commit()
+            self.db_cxn.close()
+        except AttributeError:
+            pass
+        
+    def initdb(self, tempdb, db_path=None):
+        if tempdb and not db_path:
+            tm = datetime.datetime.utcnow().isoformat(timespec='seconds')
+            db_path = os.path.join(tempfile.mkdtemp(), tm + '_test.db')
+        elif not db_path:
+            db_path = ":memory:"
+        logger.info('db_path: %s', db_path)
+        self.db_cxn = sqlite3.connect(db_path)
+        self.db_writer = self.db_cxn.cursor()
+        self.db_writer.executescript("""
+            PRAGMA foreign_keys = ON;
+            CREATE TABLE entity_enum (type TEXT(1) PRIMARY KEY);
+            INSERT INTO entity_enum (type) VALUES ('S'), ('C');
+            CREATE TABLE section_enum (type TEXT(1) PRIMARY KEY);
+            INSERT INTO section_enum (type) VALUES ('D'), ('H');
+
+            CREATE TABLE section_table (
+                id INTEGER PRIMARY KEY,
+                lineno INTEGER NOT NULL,
+                section_type TEXT(1) NOT NULL REFERENCES section_enum(type)
+            );
+            
+            CREATE TABLE section_headers (
+                id INTEGER PRIMARY KEY,
+                type_name TEXT COLLATE NOCASE,
+                raw_data TEXT NOT NULL,
+                lineno INTEGER NOT NULL,
+                fk_section INTEGER NOT NULL REFERENCES section_table(id)
+            );
+            
+            CREATE TABLE data_table (
+                id TEXT PRIMARY KEY,
+                type_name TEXT COLLATE NOCASE,
+                raw_data TEXT NOT NULL,
+                lineno INTEGER NOT NULL,
+                entity_type TEXT(1) NOT NULL REFERENCES entity_enum(type),
+                fk_section INTEGER NOT NULL REFERENCES section_table(id)
+            ) WITHOUT ROWID;
+            
+            CREATE TABLE data_xref (
+                id_from TEXT NOT NULL REFERENCES data_table(id) DEFERRABLE INITIALLY DEFERRED,
+                id_to TEXT NOT NULL REFERENCES data_table(id),
+                id_group INTEGER NOT NULL,
+                PRIMARY KEY (id_from, id_to, id_group)
+            ) WITHOUT ROWID;
+            
+            CREATE INDEX ix_type_name ON data_table(type_name);
+            CREATE INDEX ix_entity_type ON data_table(entity_type);
+            CREATE INDEX ix_fk_section ON data_table(fk_section);
+            CREATE INDEX ix_id_from ON data_xref(id_from);
+        """)
+        self.db_cxn.commit()
         
     def p_exchange_file(self, p):
         """exchange_file : PART21_START header_section data_section_list PART21_END"""
-        p[0] = P21File(p[2], p[3])
+        self.closedb()
 
     def p_header_section(self, p):
-        """header_section : HEADER header_entity header_entity header_entity ENDSEC"""
-        p[0] = P21Header(p[2], p[3], p[4])
+        """header_section : header_start header_entity header_entity header_entity ENDSEC"""
 
     def p_header_section_with_entity_list(self, p):
-        """header_section : HEADER header_entity header_entity header_entity header_entity_list ENDSEC"""
-        p[0] = P21Header(p[2], p[3], p[4])
-        p[0].extra_headers = p[5]
+        """header_section : header_start header_entity header_entity header_entity header_entity_list ENDSEC"""
+        
+    def p_header_section_start(self, p):
+        """header_start : HEADER"""
+        tmpl = "INSERT INTO section_table(lineno, section_type) VALUES (?,?)"
+        self.db_writer.execute(tmpl, (p.lineno(1), 'H'))
+        self.db_writer.execute('SELECT last_insert_rowid();')
+        (self.sid,) = self.db_writer.fetchone()
 
     def p_header_entity(self, p):
-        """header_entity : KEYWORD '(' parameter_list ')' ';'"""
-        p[0] = HeaderEntity(p[1], p[3])
-
+        """header_entity : KEYWORD raw_data ';'"""
+        tmpl = "INSERT INTO section_headers(type_name, raw_data, lineno, fk_section) VALUES (?, ?, ?, ?)"
+        self.db_writer.execute(tmpl, (p[1], p[2], p.lineno(1), self.sid))
+        
     def p_header_entity_list_init(self, p):
         """header_entity_list : header_entity"""
-        p[0] = [p[1],]
         
     def p_header_entity_list(self, p):
         """header_entity_list : header_entity_list header_entity"""
-        p[0] = p[1]
-        p[0].append(p[2])
 
     def p_data_section(self, p):
         """data_section : data_start entity_instance_list ENDSEC""" 
-        p[0] = Section(p[2])
 
     def p_data_start(self, p):
         """data_start : DATA '(' parameter_list ')' ';'"""
-        pass
+        tmpl = "INSERT INTO section_table(lineno, section_type) VALUES (?,?)"
+        lineno = p.lineno(1)
+        self.db_writer.execute(tmpl, (lineno, 'D'))
+        self.db_writer.execute('SELECT last_insert_rowid();')
+        (self.sid,) = self.db_writer.fetchone()
+        tmpl = "INSERT INTO section_headers(type_name, raw_data, lineno, fk_section) VALUES (?, ?, ?, ?)"
+        self.db_writer.executemany(tmpl, [(t, x, lineno, self.sid) for t, x in p[3]])
 
     def p_data_start_empty(self, p):
         """data_start : DATA '(' ')' ';'
                       | DATA ';'"""
-        pass
+        tmpl = "INSERT INTO section_table(lineno, section_type) VALUES (?,?)"
+        self.db_writer.execute(tmpl, (p.lineno(1), 'D'))
+        self.db_writer.execute('SELECT last_insert_rowid();')
+        (self.sid,) = self.db_writer.fetchone()        
 
     def p_data_section_list_init(self, p):
         """data_section_list : data_section"""
-        p[0] = [p[1],]
         
     def p_data_section_list(self, p):
         """data_section_list : data_section_list data_section"""
-        p[0] = p[1]
-        p[0].append(p[2])
 
     def p_entity_instance_list_init(self, p):
         """entity_instance_list : entity_instance"""
-        p[0] = [p[1],]
         
     def p_entity_instance_list(self, p):
         """entity_instance_list : entity_instance_list entity_instance"""
-        p[0] = p[1] 
-        p[0].append(p[2])
 
     def p_entity_instance(self, p):
         """entity_instance : simple_entity_instance
                            | complex_entity_instance"""
-        p[0] = p[1]
         
     def p_entity_instance_error(self, p):
         """entity_instance  : EID '=' error ';'"""
@@ -353,19 +401,21 @@ class Parser(object):
 
     def p_simple_entity_instance(self, p):
         """simple_entity_instance : EID '=' KEYWORD raw_data ';'"""
-        tmpl = "INSERT INTO data_table(id, type_name, raw_data, lineno) VALUES (?,?,?,?)"
-        self.db_writer.execute(tmpl, (p[1], p[3], p[5], p.lineno(1)))
+        eid = p[1]
+        tmpl = "INSERT INTO data_table VALUES (?,?,?,?,?,?)"
+        self.db_writer.execute(tmpl, (p[1], p[3], p[4], p.lineno(1), 'S', self.sid))
+        tmpl = "INSERT INTO data_xref(id_from, id_to, id_group) VALUES (?, ?, ?)"
+        xrefs = [(rid, eid, n) for n, x in _mkgroups(p[4]) for rid in x]
+        self.db_writer.executemany(tmpl, xrefs)
 
     def p_complex_entity_instance(self, p):
         """complex_entity_instance : EID '=' raw_data ';'"""
-        tmpl = "INSERT INTO data_table VALUES (?,NULL,?,?,?)"
-        self.db_writer.execute(tmpl, (p[1], p[4], p.lineno(1), 'C'))
-
-    def p_raw_concat(self, p):
-        """raw_data : raw_data RAW
-                    | RAW"""
-        try: p[0] = p[1] + p[2]
-        except IndexError: p[0] = p[1]
+        eid = p[1]
+        tmpl = "INSERT INTO data_table VALUES (?,NULL,?,?,?,?)"
+        self.db_writer.execute(tmpl, (p[1], p[3], p.lineno(1), 'C', self.sid))
+        tmpl = "INSERT INTO data_xref(id_from, id_to, id_group) VALUES (?, ?, ?)"
+        xrefs = [(rid, eid, n) for n, x in _mkgroups(p[3]) for rid in x]
+        self.db_writer.executemany(tmpl, xrefs)
 
     def p_parameter_list_init(self, p):
         """parameter_list : parameter"""
@@ -376,59 +426,19 @@ class Parser(object):
         p[0] = p[1]
         p[0].append(p[3])
         
-    def p_parameter_simple(self, p):
-        """parameter : STRING
-                     | INTEGER
-                     | REAL
-                     | EID
-                     | ENUMERATION
-                     | BINARY
-                     | '*'
-                     | '$'
-                     | typed_parameter
-                     | list_parameter"""
-        p[0] = p[1]
-
-    def p_list_parameter(self, p):
-        """list_parameter : '(' parameter_list ')'"""
-        p[0] = p[2]
-
     def p_typed_parameter(self, p):
-        """typed_parameter : KEYWORD '(' parameter ')'"""
-        p[0] = TypedParameter(p[1], p[3])
-
-    def p_parameter_empty_list(self, p):
-        """parameter : '(' ')'"""
-        p[0] = []
-
-    def initdb(self):
-        # TODO: memory or temp file
-        self.db_cxn = sqlite3.connect(":memory:")
-        self.db_writer = self.db_cxn.cursor()
-        self.db_writer.executescript("""
-            PRAGMA foreign_keys = ON;
-            CREATE TABLE entity_enum (type TEXT(1) PRIMARY KEY);
-            INSERT INTO entity_enum (rowid, type) VALUES (1, 'S'), (2, 'C');
-            
-            CREATE TABLE data_table (
-                id TEXT PRIMARY KEY,
-                type_name TEXT COLLATE NOCASE,
-                raw_data TEXT NOT NULL,
-                lineno INTEGER NOT NULL,
-                entity_type TEXT(1) NOT NULL DEFAULT ('S') REFERENCES entity_enum(type)
-            ) WITHOUT ROWID;
-            
-            CREATE INDEX ix_type_name ON data_table(type_name);
-            CREATE INDEX ix_entity_type ON data_table(entity_type);
-        """)
-        self.db_cxn.commit()
+        """parameter : KEYWORD raw_data"""
+        p[0] = (p[1], p[2])
         
-    def closedb(self):
-        try:
-            self.db_cxn.commit()
-            self.db_cxn.close()
-        except AttributeError:
-            pass
+    def p_other_parameter(self, p):
+        """parameter : raw_data"""
+        p[0] = (None, p[1])
+
+    def p_raw_concat(self, p):
+        """raw_data : raw_data RAW
+                    | RAW"""
+        try: p[0] = p[1] + p[2]
+        except IndexError: p[0] = p[1]
             
 
 def debug_lexer():
@@ -456,8 +466,7 @@ def debug_parser():
     logging.basicConfig()
     logger.setLevel(logging.DEBUG)
 
-    parser = Parser()
-    parser.reset()
+    parser = Parser(debug=True, tempdb=True)
     
     logger.info("***** parser debug *****")
     p = normpath(expanduser('~/projects/src/stepcode/data/ap214e3/s1-c5-214/s1-c5-214.stp'))
@@ -465,6 +474,27 @@ def debug_parser():
         s = f.read()
         parser.parse(s, debug=1)
         
+    # test reverse lookup
+    logger.info('***** testing xrefs *****')
+    tm = datetime.datetime.utcnow().isoformat(timespec='seconds')
+    db_path = os.path.join(tempfile.mkdtemp(), tm + '_xref_test.db')
+    
+    parser = Parser()
+
+    p = normpath(expanduser('~/projects/src/stepcode/data/ap214e3/s1-c5-214/s1-c5-214.stp'))
+    with codecs.open(p, 'r', encoding='iso-8859-1') as f:
+        s = f.read()
+        parser.parse(s, db_path=db_path)
+    
+    # contrived use case: we're looking for the objects referencing each of these [set] of items
+    items = [('#53','#93','#133','#173','#191'), ('#174','#192'), ('#193','#196','#195'), ('#1',)]
+    tmpl = "SELECT id_to FROM data_xref WHERE id_from IN (%s) GROUP BY id_group, id_to HAVING count(id_to) = ?"
+    with sqlite3.connect(db_path) as db_cxn:
+        for grp in items:
+            db_cursor = db_cxn.execute(tmpl % ','.join('?'*len(grp)), grp + (len(grp),))
+            for eid in db_cursor:
+                logger.info('grp: %s, ref: %r', grp, eid)
+    
     logger.info("***** finished *****")
     
 def test():
